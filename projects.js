5cab6d8b1a7e14f66a73007ad22b8a659fbb15ec
Nyi Ye Zin <nyiyezin11@gmail.com>
2023-10-24 21:20:00 +0630

    finish modal

---
+++ projects.js
@@ -0,0 +1,42 @@
+/** @format */
+
+const projects = [
+  {
+    title: 'Tonic',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet. At augue eget arcu dictum. Cras ornare arcu dui vivamus arcu felis bibendum ut. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Massa vitae tortor condimentum lacinia quis vel eros. At risus viverra adipiscing at in tellus integer. Aliquam purus sit amet luctus venenatis lectus.',
+    img: 'images/portfolio-snapshot-mobile.png',
+    metas: ['facebook', 'full stack dev', 2015],
+    stacks: ['html', 'css', 'javascript'],
+    demo: '',
+    source: '',
+  },
+  {
+    title: 'Multi-Post Stories',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet. At augue eget arcu dictum. Cras ornare arcu dui vivamus arcu felis bibendum ut. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Massa vitae tortor condimentum lacinia quis vel eros. At risus viverra adipiscing at in tellus integer. Aliquam purus sit amet luctus venenatis lectus.',
+    img: 'images/portfolio-snapshot-mobile-2.png',
+    metas: ['facebook', 'full stack dev', 2015],
+    stacks: ['html', 'css', 'javascript', 'php'],
+    demo: '',
+    source: '',
+  },
+  {
+    title: 'Uber Navigation',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet. At augue eget arcu dictum. Cras ornare arcu dui vivamus arcu felis bibendum ut. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Massa vitae tortor condimentum lacinia quis vel eros. At risus viverra adipiscing at in tellus integer. Aliquam purus sit amet luctus venenatis lectus.',
+    img: 'images/portfolio-snapshot-mobile-3.png',
+    metas: ['facebook', 'full stack dev', 2015],
+    stacks: ['html', 'css', 'javascript', 'php'],
+    demo: '',
+    source: '',
+  },
+  {
+    title: 'Facebook 360',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam sit amet. At augue eget arcu dictum. Cras ornare arcu dui vivamus arcu felis bibendum ut. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Massa vitae tortor condimentum lacinia quis vel eros. At risus viverra adipiscing at in tellus integer. Aliquam purus sit amet luctus venenatis lectus.',
+    img: 'images/portfolio-snapshot-mobile-4.png',
+    metas: ['facebook', 'full stack dev', 2015],
+    stacks: ['html', 'css', 'javascript', 'php'],
+    demo: '',
+    source: '',
+  },
+];
+
+export default projects;
